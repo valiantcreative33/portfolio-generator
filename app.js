6de3da30e3927a74411af8b7298be2b3da6d842b
Ruben Matamoros <ruben@valiantcreative.net>
2020-12-17 01:09:08 -0500

    capture user input

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log('Hello Node!');
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+console.log(profileDataArgs);
+
+const printProfileData = profileDataArr => {
+
+    profileDataArr.forEach(profileItem => console.log(profileItem));
+
+};
+
+printProfileData(profileDataArgs);
