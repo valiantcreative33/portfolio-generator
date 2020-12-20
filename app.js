6015f65f2936db96105b1e64c0e1b0726226e329
Ruben Matamoros <ruben@valiantcreative.net>
2020-12-20 18:56:27 -0500

    created html page and modularized function

--- app.js
+++ app.js
@@ -1,10 +1,32 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const fs = require('fs');
 
-const printProfileData = profileDataArr => {
+const generatePage = require('./assets/src/page-template.js');
 
-    profileDataArr.forEach(profileItem => console.log(profileItem));
+const profileDataArgs = process.argv.slice(2);
 
+const [name, github] = profileDataArgs;
+
+const generatePage = (ruben, github) => {
+  return `
+  <!DOCTYPE html> 
+  <html lang="en"> 
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Portfolio Demo</title>
+  </head>
+
+  <body>
+    <h1>${ruben}</h1>
+    <h2><a href="https://github.com/${github}">Github</a></h2>
+  </body>
+  </html>
+  `;
 };
 
-printProfileData(profileDataArgs);
+fs.writeFile('./index.html', generatePage(ruben, github), err => {
+  if (err) throw new Error(err);
+
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
