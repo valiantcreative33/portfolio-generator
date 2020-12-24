564cc842db24cde6e10a261b0a6502737622768d
Ruben Matamoros <ruben@valiantcreative.net>
2020-12-24 00:20:26 -0500

    updates to html header and about sections

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const inquirer = require('inquirer');
+const fs = require('fs');
+const generatePage = require('./assets/src/page-template'); 
 
 const promptUser = () => {
   return inquirer.prompt([
@@ -122,18 +124,13 @@ Add a New Project
 }
 
 promptUser()
-.then(promptProject)
-.then(portfolioData => {
-  console.log(portfolioData);
-});
+  .then(promptProject)
+  .then(portfolioData => {
+    const pageHTML = generatePage(portfolioData);
 
-// const fs = require('fs');
-// const generatePage = require('./src/page-template');
+    fs.writeFile('./index.html', pageHTML, err => {
+      if (err) throw new Error(err);
 
-// const pageHTML = generatePage(name, github);
-
-// fs.writeFile('./index.html', pageHTML, err => {
-//   if (err) throw err;
-
-//   console.log('Portfolio complete! Check out index.html to see the output!');
-// });
+      console.log('Page created! Check out index.html in this directory to see it!');
+    });
+  });
